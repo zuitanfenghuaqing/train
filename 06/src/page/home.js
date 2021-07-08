eda8dbe9f6fde7c864712008ecf1d55497877536
baiyike <zuitanfenghuaqing@outlook.com>
2021-07-08 17:34:57 +0800

    fix: 调整目录文件

---
+++ 06/src/page/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class Home extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    componentDidCatch(){
+
+    }
+    render(){
+        return(
+          <div>
+                <div>打哈哦啊好</div>
+          </div>
+        )
+    }                                                                                                                                                
+}
